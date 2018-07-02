0c5b387a0cda3e5a4857290be2ab605997bee142
Sonali Bansal <sonalibansal.1901@gmail.com>
2018-07-02 15:14:25 +0530

    celery functionality added

--- sendnotification.py
+++ sendnotification.py
@@ -4,6 +4,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'assignment.settings')
 django.setup()
 from assignment.celery import app
 from notification.tasks import  NotificationScheduler
+from notification.models import UserNotification
 object = app.register_task(NotificationScheduler())
-object.delay(2,6)
+
+usernotifications=UserNotification.objects.all()
+for usernotification in usernotifications:
+    object.delay(usernotification.user.id,usernotification.notification.id)
+#object.delay(2,6)
 
